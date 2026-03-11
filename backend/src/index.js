19def01ab17c645393a32bbd31c47a8aae380a82
aryansh-2003 <dixitsanjeev1975@gmail.com>
2026-03-11 12:44:46 +0530

    Utilites are created - asyncHandler, ApiResponse and ApiError

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,10 @@
 import dotenv from 'dotenv'
 import {app} from './app.js'
 import connectDB from './db/index.js'
-import dns from "node:dns"
-
-dns.setServers(["8.8.8.8","1.1.1.1"])
 
 
-dotenv.config({
-    path: '../.env'
-})
 
+dotenv.config()
 
 connectDB()
 .then(() => {
